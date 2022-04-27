bb4205779c608f27e9ad1ccd381a59d9212ad8ed
gittestcbrl <crlbelgcp@gmail.com>
2022-04-27 06:12:26 +0000

    user mgmt added

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,14 +14,17 @@ urlpatterns = [
 
     # path for contact us view
     path('contact', views.contact, name='contactpage'),
+    
     # path for registration
     path('register',views.registration_request, name='registration'),
 
     # path for login
     path('login', views.login_request, name='login'),
+    
     # path for logout
     path('logout', views.logout_request, name='logout'),
 
+    # index page
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
